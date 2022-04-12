81c1c5a8f9a2be8d50d651eb9c8e0452f4a7cecc
Matheus Melo <matheuscabal821@gmail.com>
2022-04-12 16:27:46 -0300

    Bonus n1 online persons window, and some functions

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,17 @@ function sendMessage(text){
 }
 function showPeople(){
     console.log("Mostrando pessoas e configs");
+    document.querySelector(".menu").classList.toggle("hidden");
+}
+function messageEveryone(){
+    console.log("Função mensagem para todos");
+}
+function messageTo(){
+    console.log("Função mensagem para...");
+}
+function publicMessage(){
+    console.log("função mensagem pública");
+}
+function reservedMessage(){
+    console.log("função mensagem reservada");
 }
