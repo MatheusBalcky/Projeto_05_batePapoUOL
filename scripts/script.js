1e536f92f0b3857d5183900c2e5bc3f1965b27f7
Matheus Melo <matheuscabal821@gmail.com>
2022-04-13 16:24:02 -0300

    Tela de login funcionando juntamente integra com a api

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,28 @@ function publicMessage(){
 function reservedMessage(){
     console.log("função mensagem reservada");
 }
+
+function enterRoom(){
+    function statusOnline(){
+        let statusPromise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', userName);
+        console.log('Status, enviado...')
+    }
+    function success (success) {
+        let idInterval;
+        if (success.status === 200){
+            alert("Logado com sucesso")
+            document.querySelector(".login").classList.add("hidden");
+            setInterval(statusOnline, 4500);
+        }
+        console.log("Resposta de sucesso: ", success.status);
+    }
+    function error(err){
+        alert('Nome inválido tente novamente, o nome já existe ou o campo estar vazio!');
+        console.log("Reposta de error: ", err.response.status);
+    }
+
+    let userName = {name: document.querySelector(".inputName").value};
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', userName);
+    promise.then(success);
+    promise.catch(error);
+}
